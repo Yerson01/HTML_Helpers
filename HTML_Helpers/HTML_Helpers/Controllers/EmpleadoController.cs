9d0ed6916802744f0a63b1c1aeefbd62a2c43703
Yerson01 <yersonsoto84@gmail.com>
2019-03-28 11:15:52 -0400

    Practica HTML Helpers

---
+++ HTML_Helpers/HTML_Helpers/Controllers/EmpleadoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HTML_Helpers.Models;
+
+namespace HTML_Helpers.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        // GET: Empleado
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ProcesarFormulario(Empleado emp)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Datos", emp);
+            }
+            else
+            {
+                return View("Formulario");
+            }
+        }
+
+        public ActionResult Datos(Empleado obj)
+        {
+            return View(obj);
+
+            //Estudiante datos = new Estudiante();
+
+            //datos.Nombre = Request.Form["nombre"].ToString();
+            //datos.Edad = int.Parse(Request.Form["edad"]);
+            //datos.Carrera = Request.Form["carrera"].ToString();
+            //datos.Cuatrimestre = int.Parse(Request.Form["cuatrimestre"]);
+
+
+        }
+    }
+}
---
+++ HTML_Helpers/HTML_Helpers/Models/Empleado.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HTML_Helpers.Models
+    
+{
+
+
+    public class Empleado
+    {
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public String Apellido { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public String Cedula { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [Range(15, 100, ErrorMessage = "Debes ser mayor a 15!")]
+        public int Edad { get; set; }
+
+        public String Telefono { get; set; }
+
+        
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Dirección de Correo electrónico incorrecta.")]
+        public String Correo { get; set; }
+
+        public String Genero { get; set; }
+
+        public String EstadoCivil { get; set; }
+
+        public bool Programar { get; set; }
+        public bool Dibujar { get; set; }
+        public bool Jugar { get; set; }
+        public bool Chatear { get; set; }
+
+    }
+  
+    public enum Genero
+    {
+        Masculino,
+        Femenino
+    }
+
+}
